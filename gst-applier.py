d350d9284adeba9a782cc4f28af5395d978e9c61
Prince-02Volyo <priinxe.tor@gmail.com>
2026-03-31 20:01:33 +0000

    Uploading GST Applier

---
+++ gst-applier.py
@@ -0,0 +1,18 @@
+price = int(input("Enter product price: "))
+category = input("Enter category (electronics/food items/stationary/medical): ")
+
+if category == "electronics":
+    gst = 18
+elif category == "food items":
+    gst = 8
+elif category == "stationary":
+    gst = 10        
+elif category == "medical":
+    gst = 5
+else:
+    gst = 12
+
+total_price = price + (price * gst // 100)
+
+print("GST applied:", gst, "%")
+print("Total price:", total_price)
