618a5da6f7760733822aeb5c2d007147e955f2ac
Prince <priinxe.tor@gmail.com>
2026-04-15 17:45:50 +0530

    Added another method

--- array-order.py
+++ array-order.py
@@ -1,15 +1,15 @@
 #output should be  ["","a","zz","abc","aaa"]
 
 
-def solution(inputArray):
+'''def solution(inputArray):
     return sorted(inputArray, key = len)
 
 inputArray = ["abc", "", "aaa", "a", "zz"]
-print(solution(inputArray))
+print(solution(inputArray))'''
 
 
 #def inputArray(arr1):
 
-'''arr1 = ["abc","", "aaa", "a", "zz"]
-new = sorted(arr1) 
-print(new)'''
+arr1 = ["abc","", "aaa", "a", "zz"]
+new = sorted(arr1, key =len) 
+print(new)
